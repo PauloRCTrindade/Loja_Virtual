10e0289ce165a285855bef4d79d41362e0ac7ee5
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-21 05:00:32 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const routes  = express.Router()
-const membro = require('./app/controllers/membros')
-const valor = require('./app/controllers/valores')
-
 
 routes.get('/',function(req,res){
-  return res.send('OK')
+  return res.render('layout.njk')
 })
 
 
